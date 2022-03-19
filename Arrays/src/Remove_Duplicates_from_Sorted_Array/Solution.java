6f60b998f893910b0c2b21e852cda756092d5707
Mahesh Mokale <mmokale@whipmedia.com>
2022-03-19 23:19:01 +0530

    Remove duplicates from array

---
+++ Arrays/src/Remove_Duplicates_from_Sorted_Array/Solution.java
@@ -0,0 +1,20 @@
+package Remove_Duplicates_from_Sorted_Array;
+
+public class Solution {
+
+    public static void main(String[] args){
+        Solution solution=new Solution();
+        int[] nums={0,0,1,1,1,2,2,3,3,4};
+        System.out.println(solution.removeDuplicates(nums));
+    }
+    public int removeDuplicates(int[] nums) {
+        int index=1;
+        for(int i=0;i< nums.length-1;i++){
+            if(nums[i]!=nums[i+1]) {
+                nums[index++] = nums[i + 1];
+            }
+        }
+        return index;
+    }
+
+}
