2c06f6f0110c8ccb4c97fa79d6d4bec6aefea65d
Mahesh Mokale <mmokale@whipmedia.com>
2022-03-21 22:59:32 +0530

    Remove Element

---
+++ Arrays/src/Remove_Element/Solution.java
@@ -0,0 +1,20 @@
+package Remove_Element;
+
+public class Solution {
+    public static void main(String[] args){
+        Solution solution=new Solution();
+        int[]nums={3,2,2,3};
+        System.out.println(solution.removeElement(nums,3));
+    }
+    public int removeElement(int[] nums, int val) {
+        if(nums.length==0) return 0;
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+    return j;
+    }
+}
