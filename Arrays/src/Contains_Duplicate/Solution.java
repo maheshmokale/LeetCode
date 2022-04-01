0da4b277b86cea7e119932ce4f2b1b399ee40fee
Mahesh Mokale <mmokale@whipmedia.com>
2022-04-02 00:23:50 +0530

    Contains_Duplicate

--- Arrays/src/Contains_Duplicate/Solution.java
+++ Arrays/src/Contains_Duplicate/Solution.java
@@ -4,6 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
+    public static void main(String[]args){
+        int[] nums={1,2,3,1};
+        Solution solution= new Solution();
+        solution.containsDuplicate(nums);
+    }
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> setNums=new HashSet<>();
         for(int val:nums){
---
+++ Arrays/src/Contains_Duplicate_II/Solution.java
@@ -0,0 +1,26 @@
+package Contains_Duplicate_II;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public static void main(String[] args){
+        int[] nums={1,2,3,1,2,3};
+        int k=2;
+        Solution solution = new Solution();
+        System.out.println(solution.containsNearbyDuplicate(nums,k));
+    }
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> duplicateHolder=new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            if(!duplicateHolder.containsKey(nums[i]))
+                duplicateHolder.put(nums[i],i);
+            else
+                if(Math.abs(i-duplicateHolder.get(nums[i]))<=k)
+                    return true;
+                else
+                    duplicateHolder.put(nums[i],i);
+        }
+        return false;
+    }
+}
