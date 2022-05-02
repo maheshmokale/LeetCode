db64e9a7ebe9e8db3dd32c47c2f7074812d89aed
Mahesh Mokale <mmokale@whipmedia.com>
2022-05-02 20:11:01 +0530

    Valid_Parentheses

---
+++ String/src/Valid_Parentheses/Solution.java
@@ -0,0 +1,26 @@
+package Valid_Parentheses;
+
+import java.util.*;
+
+class Solution {
+    Map<String,Integer> paranthVal=new HashMap<>();
+    public boolean isValid(String s) {
+        if(s.length()%2!=0) return false;
+        Stack<Character> stack=new Stack<>();
+        for(char paranth : s.toCharArray()){
+            if(paranth =='(' || paranth =='{' || paranth =='['){
+                stack.push(paranth);
+            }
+            else if(stack.isEmpty()) return false;
+            else if(paranth ==')' &&  stack.pop() !='(') return false;
+            else if(paranth =='}' &&  stack.pop() !='{') return false;
+            else if(paranth ==']' &&  stack.pop() !='[') return false;
+        }
+    return stack.isEmpty();
+    }
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String paranthese="([}}])";
+        System.out.println(solution.isValid(paranthese));
+    }
+}
