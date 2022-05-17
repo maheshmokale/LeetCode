2b8830899c8431e42d1805d88cb952eeedc7f5ae
Mahesh Mokale <mmokale@whipmedia.com>
2022-05-17 23:49:12 +0530

    Remove_Linked_List_Elements

---
+++ LinkedList/src/Remove_Linked_List_Elements/Solution.java
@@ -0,0 +1,51 @@
+package Remove_Linked_List_Elements;
+
+
+ // Definition for singly-linked list.
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null) return null;
+        while(head!=null && head.val==val)
+            head=head.next;
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            if(temp.next.val==val){
+                temp.next=temp.next.next;
+            }
+            else
+                temp=temp.next;
+        }
+        return head;
+    }
+    public static void main(String[] args){
+        Solution solution=new Solution();
+        ListNode head=createLinkedList(new int[]{7,7,7,7});
+        int val=7;
+        solution.removeElements(head ,val);
+    }
+    public static ListNode createLinkedList(int[] list){
+        ListNode temp=null;
+        ListNode result=null;
+
+        for (int val:list) {
+            if(temp==null){
+                temp=new ListNode(val);
+                result=temp;
+                continue;
+            }
+            else
+                temp.next=new ListNode(val);
+            temp=temp.next;
+
+        }
+        return result;
+    }
+}
