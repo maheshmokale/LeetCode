12e957fbb1d5807dd6ca6ba0d671b7292ee52348
Mahesh Mokale <mmokale@whipmedia.com>
2022-05-23 22:57:51 +0530

    Container_With_Most_Water

---
+++ Arrays/src/Container_With_Most_Water/Solution.java
@@ -0,0 +1,23 @@
+package Container_With_Most_Water;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        int maxArea=Integer.MIN_VALUE;
+        while(i<j){
+            int area=(Math.abs(i-j)*Math.min(height[i],height[j]));
+            maxArea=Math.max(maxArea,area);
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return maxArea;
+    }
+    public static void main(String[] args){
+        Solution solution=new Solution();
+        int[] heights={1,1};
+        System.out.println(solution.maxArea(heights));
+    }
+}
