f3897b7123ed1080c72661b45d3623971419de98
maheshmokale <maheshmokale.mm@gmail.com>
2023-04-10 21:13:19 -0700

    Valid_Parentheses

--- String/src/Easy/Valid_Parentheses/Solution.java
+++ String/src/Easy/Valid_Parentheses/Solution.java
@@ -1,6 +1,8 @@
 package Easy.Valid_Parentheses;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 class Solution {
     Map<String,Integer> paranthVal=new HashMap<>();
@@ -20,7 +22,29 @@ class Solution {
     }
     public static void main(String[] args){
         Solution solution = new Solution();
-        String paranthese="([}}])";
-        System.out.println(solution.isValid(paranthese));
+        String paranthese="){";
+       // System.out.println(solution.isValid(paranthese));
+        System.out.println(solution.isValid1(paranthese));
+    }
+
+    public boolean isValid1(String s) {
+        if(s==null || s.length()<2) return false;
+        if(s.length()%2!=0) return false;
+        int i=0;
+
+        Stack<Character> stackStore=new Stack<>();
+        while(i<s.length()){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' ||s.charAt(i)=='['){
+                stackStore.push(s.charAt(i));
+            }
+            if(stackStore.isEmpty()) return false;
+            else if(s.charAt(i)==')' && stackStore.pop()!='(') return  false;
+            else if(s.charAt(i)==']' && stackStore.pop()!='[') return  false;
+            else if(s.charAt(i)=='}' && stackStore.pop()!='{') return  false;
+
+            i++;
+        }
+        if(!stackStore.isEmpty()) return false;
+        return true;
     }
 }
