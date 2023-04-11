1945c07aac6a46d1ab78138b60283ff18dfb4e53
maheshmokale <maheshmokale.mm@gmail.com>
2023-04-10 21:13:00 -0700

    Longest_Common_Prefix

--- String/src/Easy/Longest_Common_Prefix/Solution.java
+++ String/src/Easy/Longest_Common_Prefix/Solution.java
@@ -20,9 +20,38 @@ class Solution {
         }
         return commonPre;
     }
+    public static String  longestCommonPrefix1(String[] strs) {
+        if(strs==null || strs.length==0) return null;
+        if(strs.length==1) return strs[0];
+        int i=1;
+        String temp = null;
+        String commonPrefix=strs[0];
+        while(i<strs.length){
+            int j=0;
+            while(j<commonPrefix.length() && j<strs[i].length()){
+                if (commonPrefix.charAt(j)==strs[i].charAt(j)){
+                    if(temp==null)
+                        temp=String.valueOf(commonPrefix.charAt(j));
+                    else
+                        temp=temp.concat(String.valueOf(commonPrefix.charAt(j)));
+                }
+                else{
+                    break;
+                }
+                j++;
+            }
+            commonPrefix=temp;
+            if(commonPrefix==null) return "";
+            temp=null;
+            i++;
+        }
+        return commonPrefix;
+    }
+
     public static void main(String[] args){
         Solution solution = new Solution();
-        String[] strs={"cir","car"};
-        System.out.println(solution.longestCommonPrefix(strs));
+        String[] strs={"flower","flow","flight"};
+        //System.out.println(solution.longestCommonPrefix(strs));
+        System.out.println(longestCommonPrefix1(strs));
     }
 }
